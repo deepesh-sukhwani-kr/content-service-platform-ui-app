dc86435d14c0ea83cda7be30a69ef33a4b9343dc
tonmatos <wanilton.filho@kroger.com>
2023-03-22 11:45:34 -0400

    Fixing sonar issues

--- contentservicesplatform-ui-app-server/src/main/java/com/kroger/csp/ui/converter/v1/AddImageRequestConverter.java
+++ contentservicesplatform-ui-app-server/src/main/java/com/kroger/csp/ui/converter/v1/AddImageRequestConverter.java
@@ -23,6 +23,7 @@ import java.util.ArrayList;
 import java.util.Base64;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Stream;
 
 /**
  * CSP Add image V1 API request converter class
@@ -48,61 +49,32 @@ public class AddImageRequestConverter {
     }
 
     /**
-     * Populate Asset Details List
-     * @param requestList
-     * @return List of AssetDetails object
-     * @throws Exception
-     */
-    private List<AssetDetails> populateAssetDetailsList(List<AssetDetailsUIRequest> requestList) throws Exception {
-        List<AssetDetails> assetDetailsList = new ArrayList<>();
-        for(AssetDetailsUIRequest request : requestList){
-            assetDetailsList.add(populateAssetDetails(request));
-        }
-        return assetDetailsList;
-    }
-
-    /**
      * Populate Transaction ref
+     *
      * @param request
      * @return TransactionRef object
      */
-    private TransactionRef populateTransactionRef(AddImageUIRequest request){
+    private TransactionRef populateTransactionRef(AddImageUIRequest request) {
         TransactionRef transactionRef = new TransactionRef();
         transactionRef.setReferenceID(request.getReferenceId());
         transactionRef.setCreationdatetime(request.getCreationDatetime());
         transactionRef.setEvent("ADDASSET");
         transactionRef.setSource("UI");
-        if("stage".equalsIgnoreCase(env))
-            transactionRef.setEnvironment("test");
-        else if("local".equalsIgnoreCase(env))
-            transactionRef.setEnvironment("test");
-        else
+        if ("stage".equalsIgnoreCase(env)) {transactionRef.setEnvironment("test");} else if ("local".equalsIgnoreCase(
+                env)) {transactionRef.setEnvironment("test");} else {
             transactionRef.setEnvironment(env);
+        }
         return transactionRef;
     }
 
     /**
-     * Populate individual AssetDetails objects
-     * @param request
-     * @return AssetDetails object
-     * @throws Exception
-     */
-    private AssetDetails populateAssetDetails(AssetDetailsUIRequest request) throws Exception{
-        AssetDetails assetDetails = new AssetDetails();
-        assetDetails.setSequence(request.getSequence());
-        assetDetails.setFileName(request.getFileName());
-        assetDetails.setAttributeMap(populateAttributeMap(request));
-        assetDetails.setAssetInfo(populateAssetInfo(request));
-        return assetDetails;
-    }
-
-    /**
      * Populate Asset object
+     *
      * @param request
      * @return Asset object
      * @throws Exception
      */
-    private Asset populateAsset(AddImageUIRequest request) throws Exception{
+    private Asset populateAsset(AddImageUIRequest request) throws Exception {
         Asset asset = new Asset();
         asset.setImageType(request.getImageType());
         asset.setItemType("ProductImages");
@@ -112,11 +84,43 @@ public class AddImageRequestConverter {
     }
 
     /**
+     * Populate Asset Details List
+     *
+     * @param requestList
+     * @return List of AssetDetails object
+     * @throws Exception
+     */
+    private List<AssetDetails> populateAssetDetailsList(List<AssetDetailsUIRequest> requestList) throws Exception {
+        List<AssetDetails> assetDetailsList = new ArrayList<>();
+        for (AssetDetailsUIRequest request : requestList) {
+            assetDetailsList.add(populateAssetDetails(request));
+        }
+        return assetDetailsList;
+    }
+
+    /**
+     * Populate individual AssetDetails objects
+     *
+     * @param request
+     * @return AssetDetails object
+     * @throws Exception
+     */
+    private AssetDetails populateAssetDetails(AssetDetailsUIRequest request) throws Exception {
+        AssetDetails assetDetails = new AssetDetails();
+        assetDetails.setSequence(request.getSequence());
+        assetDetails.setFileName(request.getFileName());
+        assetDetails.setAttributeMap(populateAttributeMap(request));
+        assetDetails.setAssetInfo(populateAssetInfo(request));
+        return assetDetails;
+    }
+
+    /**
      * Puplate Attribute map
+     *
      * @param request
      * @return AttributeMap object
      */
-    private AttributeMap populateAttributeMap(AssetDetailsUIRequest request){
+    private AttributeMap populateAttributeMap(AssetDetailsUIRequest request) {
         AttributeMap attributeMap = new AttributeMap();
         attributeMap.setIMP_VIEW_ANGLE(request.getViewAngle());
         attributeMap.setIMP_PROVIDED_SIZE(request.getProvidedSize());
@@ -125,10 +129,9 @@ public class AddImageRequestConverter {
         attributeMap.setIMP_IMAGE_LAST_MODIFIED_DT(request.getLastModifiedDate());
         attributeMap.setIMP_DESCRIPTION(request.getDescription());
         attributeMap.setIMP_FILE_TYPE_EXT(request.getFileExtension());
-        if(StringUtils.isNotBlank(request.getColorProfile()))
+        if (StringUtils.isNotBlank(request.getColorProfile())) {
             attributeMap.setIMP_COLOR_REP(request.getColorProfile());
-        else
-            attributeMap.setIMP_COLOR_REP("RGB");
+        } else {attributeMap.setIMP_COLOR_REP("RGB");}
         attributeMap.setIMP_UPC10(request.getUpc10());
         attributeMap.setIMP_UPC12(request.getUpc12());
         attributeMap.setIMP_UPC13(request.getUpc13());
@@ -138,42 +141,39 @@ public class AddImageRequestConverter {
 
     /**
      * Populate AssetInfo object
+     *
      * @param request
      * @return AssetInfo object
      * @throws Exception
      */
-    private AssetInfo populateAssetInfo(AssetDetailsUIRequest request) throws Exception{
+    private AssetInfo populateAssetInfo(AssetDetailsUIRequest request) throws Exception {
         AssetInfo assetInfo = new AssetInfo();
         assetInfo.setAssetType(request.getAssetType());
-        if (StringUtils.isNotBlank(request.getFilePath())){
+        if (StringUtils.isNotBlank(request.getFilePath())) {
             Path path = Paths.get(request.getFilePath());
-            Optional<Path> filePath = Files.find(path, 1,
-                    (path1, basicFileAttributes) ->
-                            path1.toFile().getName().contains(request.getFileName())).limit(1).findFirst();
-            if (filePath.isPresent())
-                assetInfo.setAsset(getAssetAsString(filePath));
-        }else
+            try (Stream<Path> filesStream = Files.find(path, 1,
+                    (path1, basicFileAttributes) -> path1.toFile().getName().contains(request.getFileName()))) {
+                Optional<Path> filePath = filesStream.limit(1).findFirst();
+                setAssetInfoAsset(assetInfo, filePath);
+            }
+        } else {
             assetInfo.setAsset(request.getAsset());
+        }
         return assetInfo;
     }
 
-    /**
-     * Get Asset and Base64 encoded string
-     * @param filePath
-     * @return Base 64 encoded asset String
-     * @throws Exception
-     */
-    private String getAssetAsString(Optional<Path> filePath) throws Exception{
-        File imageFile = new File(filePath.get().toAbsolutePath().toString());
-        try (FileInputStream fis = new FileInputStream(imageFile)) {
-            ByteArrayOutputStream output = new ByteArrayOutputStream();
-            byte[] buf = new byte[1024];
-            for (int n; (n = fis.read(buf)) != -1; ) {
-                output.write(buf, 0, n);
+    private void setAssetInfoAsset(AssetInfo assetInfo, Optional<Path> filePath) throws Exception {
+        if (filePath.isPresent()) {
+            File imageFile = new File(filePath.get().toAbsolutePath().toString());
+            try (FileInputStream fis = new FileInputStream(imageFile)) {
+                ByteArrayOutputStream output = new ByteArrayOutputStream();
+                byte[] buf = new byte[1024];
+                for (int n; (n = fis.read(buf)) != -1; ) {
+                    output.write(buf, 0, n);
+                }
+                byte[] imgBytes = output.toByteArray();
+                assetInfo.setAsset(Base64.getEncoder().encodeToString(imgBytes));
             }
-            byte[] imgBytes = output.toByteArray();
-            return Base64.getEncoder().encodeToString(imgBytes);
         }
     }
-
 }
